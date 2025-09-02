17c3fb3d5e2d076a40a207d28c8ce16b6816e610
bala-oss <balathecomdein@gmail.com>
2025-09-02 22:17:54 +0530

    Update Home.js

--- Home.js
+++ Home.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate, useLocation, useNavigate } from "react-router-dom";
 import Home from "./Components/Home";
 
 import axios from "axios";
