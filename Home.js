c6c9984c1b3e0f4735482e8f97c9ba60562eae94
bala-oss <balathecomdein@gmail.com>
2025-09-03 08:43:53 +0530

    Update Home.js

--- Home.js
+++ Home.js
@@ -1,3 +1,13 @@
-export default function Home() {
-  return <h1>Welcome to Home Page</h1>;
-}
+import React from 'react';
+import './Home.css'; 
+
+const Home = () => {
+  return (
+    <div className="home-container">
+      <h2>Welcome to the Home Page!</h2>
+      <p>This is a simple homepage for our application.</p>
+    </div>
+  );
+};
+
+export default Home;
