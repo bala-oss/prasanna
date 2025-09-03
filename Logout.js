0cd23a7194844816450da5b124308ffc39b7e282
bala-oss <balathecomdein@gmail.com>
2025-09-03 09:37:48 +0530

    Update Logout.js

--- Logout.js
+++ Logout.js
@@ -1,13 +1,19 @@
-import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
-export default function Logout({ setIsLoggedIn }) {
+const Logout = ({ handleLogout }) => {
   const navigate = useNavigate();
 
   useEffect(() => {
-    setIsLoggedIn(false);
-    navigate("/");
-  }, [setIsLoggedIn, navigate]);
+    handleLogout(); // Clear token and update auth state in App.js
+    navigate('/login'); // Redirect to login page
+  }, [handleLogout, navigate]);
 
-  return <h2>Logging out...</h2>;
-}
+  return (
+    <div>
+      <h2>Logging out...</h2>
+    </div>
+  );
+};
+
+export default Logout;
