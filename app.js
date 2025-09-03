34b319d4c5a6525798a92314cc5da06e80cb13b5
bala-oss <balathecomdein@gmail.com>
2025-09-03 21:18:40 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,12 +5,20 @@ import Body from "./componets/body";
 function App() {
   const [user, setUser] = useState(null);
   const [view, setView] = useState("home"); // home | login | signup
+  const [registeredUsers, setRegisteredUsers] = useState([]); // New state to store registered usernames
 
   return (
     <div>
       <Head user={user} setView={setView} setUser={setUser} />
-      <Body user={user} setUser={setUser} view={view} setView={setView} />
-      <footer style={{ textAlign: "center", marginTop: 40, padding: 20, background: "#eee" }}>
+      <Body
+        user={user}
+        setUser={setUser}
+        view={view}
+        setView={setView}
+        registeredUsers={registeredUsers} // Pass registeredUsers to Body
+        setRegisteredUsers={setRegisteredUsers} // Pass setRegisteredUsers to Body
+      />
+      <footer style={{ textAlign: "center", marginTop: 40, padding: 20, background: "#eee", color: "#333", borderTop: "1px solid #ddd" }}>
         &copy; 2025 Your Company
       </footer>
     </div>
